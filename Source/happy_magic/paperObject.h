f4f06925a320ecb4ecc50afab084ad05181b01d0
NetFoley <netfoley@gmail.com>
2021-11-22 17:58:43 +0100

    staff

---
+++ Source/happy_magic/paperObject.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+#include "paperObject.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HAPPY_MAGIC_API ApaperObject : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+};
